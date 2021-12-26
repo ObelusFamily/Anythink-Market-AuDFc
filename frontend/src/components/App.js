cb2adde59c237b3a72e3c4e4f6c64effedc58ef7
Limor <limor.mekaiten@gmail.com>
2021-12-26 13:39:13 +0200

    Update App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -40,12 +40,12 @@ class App extends React.Component {
   }
 
   componentWillMount() {
-    const token = window.localStorage.getItem("jwt");
+    var token = window.localStorage.getItem("jwt");
     if (token) {
       agent.setToken(token);
     }
 
-    this.props.onLoad(token ? agent.Auth.current() : null, token);
+    this.props.onLoad(token ? agent.Auth.current() :  null, token);
   }
 
   render() {
